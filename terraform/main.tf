22a118815a6d2cccdbc9024770043dae210c2b5e
Jenkins <jenkins@ip-172-31-45-208.eu-north-1.compute.internal>
2025-07-06 18:27:32 +0000

    Updated for IAC tool

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "Server1" {
+  ami                    = "ami-09a9858973b288bdd"
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.web.id
+  vpc_security_group_ids = [aws_security_group.ssh-access.id]
+}
+
+
+
+resource "aws_key_pair" "web" {
+  public_key = file("/var/lib/jenkins/terraform_test/cred/id_rsa.pub")
+}
+
+resource "aws_security_group" "ssh-access" {
+  name = "ssh-access"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.Server1.public_ip
+}
+
+resource "null_resource" "command" {
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.Server1.public_ip}>>/var/lib/jenkins/terraform_test/cred/inventory"
+  }
+}
---
+++ terraform/readme.txt
@@ -0,0 +1,9 @@
+
+Hello before running this please make sure to configure your aws cunfigurations , like , Acces ID , Access Key , Region. using aws configure command.
+Propmt will be like below :- 
+AWS Access Key ID [None]: <Your access ID>
+AWS Secret Access Key [None]: <Your Access key>
+Default region name [None]: ap-south-1
+
+Alternatively , you can create one provider.tf file with  these details to avoid terraform failure.
+make sure to have key at the given place in terraform main.tf file .
