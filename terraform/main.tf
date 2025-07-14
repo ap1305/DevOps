6ea221b36bf2d44c1ea39b369bbff7155c6b4a79
ap1305 <66279680+ap1305@users.noreply.github.com>
2025-07-14 22:42:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,13 @@ resource "aws_security_group" "ssh-access" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 output "public_ip" {
