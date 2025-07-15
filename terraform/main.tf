2096a3fdae45b66cb3680867a509c3d67de49248
Anish Prakash <ap1305>
2025-07-15 09:58:43 +0000

    Updated Terraform for port opening on 8000

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,14 @@ resource "aws_key_pair" "web" {
 
 resource "aws_security_group" "ssh-access" {
   name = "ssh-access"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ingress_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
   egress {
     from_port        = 0
